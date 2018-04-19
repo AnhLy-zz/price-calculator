4ea781dd2ffab435c15a6488ad09c1743bcab6b5
Nick Anh Ly <nhatanhvl@gmail.com>
2018-04-20 02:06:20 +0700

    Update pages

---
+++ src/app/data/sofa.js
@@ -0,0 +1,279 @@
+let products = [
+  {
+    productId: 'SG95',
+    type: '51195',
+    name: 'Sofa góc SG95',
+    image: '',
+    features: 'Sofa góc',
+    size: {
+      length: '2810',
+      width: '1981',
+      height: '845'
+    },
+    price: {
+      standard: '17780000',
+      requestNormal: '19980000',
+      requestHighLevel: '23380000',
+      requestCowSkinContact: '37780000',
+      requestFullCowSkin: '52280000'
+    },
+    amplitude: {
+      pu: '1500000',
+      cowSkin: '5000000'
+    }
+  },
+  {
+    productId: 'SG09',
+    type: '9909',
+    name: 'Sofa góc SG09',
+    image: '',
+    features: 'Sofa góc',
+    size: {
+      length: '2810',
+      width: '2120',
+      height: '845'
+    },
+    price: {
+      standard: '17480000',
+      requestNormal: '19680000',
+      requestHighLevel: '22980000',
+      requestCowSkinContact: '37480000',
+      requestFullCowSkin: '51880000'
+    },
+    amplitude: {
+      pu: '1500000',
+      cowSkin: '5000000'
+    }
+  },
+  {
+    productId: 'SG25',
+    type: '1425',
+    name: 'Sofa góc SG25',
+    image: '',
+    features: 'Sofa góc',
+    size: {
+      length: '2300',
+      width: '1600',
+      height: '500'
+    },
+    price: {
+      standard: '10980000',
+      requestNormal: '13280000',
+      requestHighLevel: '15480000',
+      requestCowSkinContact: '25480000',
+      requestFullCowSkin: '35480000'
+    },
+    amplitude: {
+      pu: '500000',
+      cowSkin: '3000000'
+    }
+  },
+  {
+    productId: 'SG23',
+    type: '023',
+    name: 'Sofa góc SG23',
+    image: '',
+    features: 'Sofa góc',
+    size: {
+      length: '2200',
+      width: '1600',
+      height: '440'
+    },
+    price: {
+      standard: '12980000',
+      requestNormal: '12980000',
+      requestHighLevel: '15280000',
+      requestCowSkinContact: '25280000',
+      requestFullCowSkin: '35280000'
+    },
+    amplitude: {
+      pu: '',
+      cowSkin: '3000000'
+    }
+  },
+  {
+    productId: 'SB23',
+    type: '023',
+    name: 'Sofa băng SB23',
+    image: '',
+    features: 'Sofa băng',
+    size: {
+      length: '2200',
+      width: '900',
+      height: '900'
+    },
+    price: {
+      standard: '11280000',
+      requestNormal: '11280000',
+      requestHighLevel: '0',
+      requestCowSkinContact: '0',
+      requestFullCowSkin: '0'
+    },
+    amplitude: {
+      pu: '0',
+      cowSkin: '0'
+    }
+  },
+  {
+    productId: 'SG29',
+    type: '029',
+    name: 'Sofa góc SG29',
+    image: '',
+    features: 'Sofa góc',
+    size: {
+      length: '2200',
+      width: '1600',
+      height: '440'
+    },
+    price: {
+      standard: '14380000',
+      requestNormal: '14380000',
+      requestHighLevel: '16580000',
+      requestCowSkinContact: '26580000',
+      requestFullCowSkin: '36580000'
+    },
+    amplitude: {
+      pu: '0',
+      cowSkin: '3000000'
+    }
+  },
+  {
+    productId: 'SG43',
+    type: '043',
+    name: 'Sofa góc SG43',
+    image: '',
+    features: 'Sofa góc',
+    size: {
+      length: '2810',
+      width: '1600',
+      height: '500'
+    },
+    price: {
+      standard: '17780000',
+      requestNormal: '17780000',
+      requestHighLevel: '19980000',
+      requestCowSkinContact: '32880000',
+      requestFullCowSkin: '43380000'
+    },
+    amplitude: {
+      pu: '0',
+      cowSkin: '4000000'
+    }
+  },
+  {
+    productId: 'SET38',
+    type: '038',
+    name: 'Bộ sofa SET38',
+    image: '',
+    features: 'Sofa băng + 2 Đơn',
+    size: {
+      length: '',
+      width: '',
+      height: ''
+    },
+    price: {
+      standard: '18880000',
+      requestNormal: '18880000',
+      requestHighLevel: '21180000',
+      requestCowSkinContact: '35580000',
+      requestFullCowSkin: '47780000'
+    },
+    amplitude: {
+      pu: '0',
+      cowSkin: '3000000'
+    }
+  },
+  {
+    productId: 'SET12',
+    type: '012',
+    name: 'Bộ sofa S012',
+    image: '',
+    features: 'Sofa băng + 2 Đơn',
+    size: {
+      length: '',
+      width: '',
+      height: ''
+    },
+    price: {
+      standard: '18880000',
+      requestNormal: '18880000',
+      requestHighLevel: '21180000',
+      requestCowSkinContact: '35580000',
+      requestFullCowSkin: '47780000'
+    },
+    amplitude: {
+      pu: '500000',
+      cowSkin: '3000000'
+    }
+  },
+  {
+    productId: 'SET05',
+    type: '005',
+    name: 'Bộ sofa S005 màu kem',
+    image: '',
+    features: 'Sofa băng + 2 Đơn',
+    size: {
+      length: '',
+      width: '',
+      height: ''
+    },
+    price: {
+      standard: '18880000',
+      requestNormal: '18880000',
+      requestHighLevel: '21180000',
+      requestCowSkinContact: '35580000',
+      requestFullCowSkin: '47780000'
+    },
+    amplitude: {
+      pu: '',
+      cowSkin: '3000000'
+    }
+  },
+  {
+    productId: 'SG15A',
+    type: '115A',
+    name: 'Sofa góc SG15A',
+    image: '',
+    features: 'Sofa góc',
+    size: {
+      length: '2810',
+      width: '1650',
+      height: '440'
+    },
+    price: {
+      standard: '17780000',
+      requestNormal: '17780000',
+      requestHighLevel: '19480000',
+      requestCowSkinContact: '34480000',
+      requestFullCowSkin: '48380000'
+    },
+    amplitude: {
+      pu: '',
+      cowSkin: '5000000'
+    }
+  },
+  {
+    productId: 'SB15',
+    type: '115',
+    name: 'Sofa băng SB15',
+    image: '',
+    features: 'Sofa băng',
+    size: {
+      length: '2000',
+      width: '830',
+      height: '800'
+    },
+    price: {
+      standard: '9480000',
+      requestNormal: '9480000',
+      requestHighLevel: '10580000',
+      requestCowSkinContact: '19480000',
+      requestFullCowSkin: '26180000'
+    },
+    amplitude: {
+      pu: '',
+      cowSkin: '1000000'
+    }
+  },
+  
+]
